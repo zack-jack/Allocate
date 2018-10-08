93f3e2c0be2dd69594bdd11e0a9a3f13a75d0178
zackjacksongit <z.jackson58@gmail.com>
2018-10-08 19:24:20 -0400

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+
+const AllocateDashboardPage = () => (
+  <div>This is from my dashboard component.</div>
+);
+
+const AddExpensePage = () => <div>This is from my add expense component.</div>;
+
+const EditExpensePage = () => (
+  <div>This is from my edit expense component.</div>
+);
+
+const HelpPage = () => <div>This is from my help component.</div>;
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={AllocateDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("app"));
