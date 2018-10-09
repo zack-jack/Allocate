7a40a8d2106a7bc9868748be04c8e430b42dc918
zackjacksongit <z.jackson58@gmail.com>
2018-10-09 18:45:52 -0400

    Reducers for expenses and filters

--- src/redux.js
+++ src/redux.js
@@ -1,17 +1,57 @@
 import { createStore, combineReducers } from "redux";
 
-const State = {
-  expenses = [{
-    id: '',
-    description: '',
-    note: '',
-    amount: 0,
-    createdAt: 0
-  }],
+// ADD_EXPENSE
+// REMOVE_EXPENSE
+// EDIT_EXPENSE
+// SET_TEXT_FILTER
+// SORT_BY_DATE
+// SORT_BY_AMOUNT
+// SET_START_DATE
+// SET_END_DATE
+
+const expensesReducerDefaultState = [];
+const filtersReducerDefaultState = {
+  text: "",
+  sortBy: "date",
+  startDate: undefined,
+  endDate: undefined
+};
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const store = createStore(
+  combineReducers({
+    expenses: expensesReducer,
+    filters: filtersReducer
+  })
+);
+
+const state = {
+  expenses: [
+    {
+      id: "",
+      description: "",
+      note: "",
+      amount: 0,
+      createdAt: 0
+    }
+  ],
   filters: {
-    text: '',
-    sortBy: '',
+    text: "",
+    sortBy: "",
     startDate: undefined,
     endDate: undefined
   }
-}
+};
