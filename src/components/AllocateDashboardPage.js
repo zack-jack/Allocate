8fb179018b95e572dee938addc35d16b4c2b9e05
zackjacksongit <z.jackson58@gmail.com>
2018-10-08 20:28:23 -0400

    AppRouter and components broken out

---
+++ src/components/AllocateDashboardPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AllocateDashboardPage = () => (
+  <div>This is from my dashboard component.</div>
+);
+
+export default AllocateDashboardPage;
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import AddExpensePage from "../components/AddExpensePage";
+import AllocateDashboardPage from "../components/AllocateDashboardPage";
+import EditExpensePage from "../components/EditExpensePage";
+import Header from "../components/Header";
+import HelpPage from "../components/HelpPage";
+import NotFoundPage from "../components/NotFoundPage";
+
+const AppRouter = () => (
+  <BrowserRouter>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={AllocateDashboardPage} exact={true} />
+        <Route path="/create" component={AddExpensePage} />
+        <Route path="/edit" component={EditExpensePage} />
+        <Route path="/help" component={HelpPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
+
+export default AppRouter;
