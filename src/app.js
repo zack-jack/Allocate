8cbcee58163380d68dba0de7245d9cd71a91ce6c
zackjacksongit <z.jackson58@gmail.com>
2018-10-09 23:43:48 -0400

    Redux refactor to break out to multiple files

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { addExpense } from "./actions/expenses";
+import { setTextFilter } from "./actions/filters";
+import getVisibleExpenses from "./selectors/expenses";
 import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
 
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
+
+const store = configureStore();
+
+store.dispatch(addExpense({ description: "Water bill" }));
+store.dispatch(addExpense({ description: "Gas bill" }));
+store.dispatch(setTextFilter("water"));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
 
 ReactDOM.render(<AppRouter />, document.getElementById("app"));
