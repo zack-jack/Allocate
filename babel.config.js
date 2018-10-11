d0f2d344f1eba0c0737e075eb6992e7376ce2bc3
zackjacksongit <z.jackson58@gmail.com>
2018-10-10 22:25:56 -0400

    Jest setup and expenses actions tests

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+"use strict";
+
+module.exports = {
+  presets: ["@babel/preset-env", "@babel/preset-react"],
+  plugins: [
+    "@babel/plugin-proposal-class-properties",
+    "@babel/plugin-syntax-object-rest-spread"
+  ]
+};
