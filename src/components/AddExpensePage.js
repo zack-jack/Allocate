5cc531c2bd1f93f6950ce4e6797aac8659bf98fd
zackjacksongit <z.jackson58@gmail.com>
2018-10-12 11:49:05 -0400

    AddExpensePage tests correction

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -6,7 +6,7 @@ import { addExpense } from "../actions/expenses";
 
 export class AddExpensePage extends Component {
   onSubmit = expense => {
-    this.props.onSubmit(expense);
+    this.props.addExpense(expense);
     this.props.history.push("/");
   };
 
@@ -21,7 +21,7 @@ export class AddExpensePage extends Component {
 }
 
 const mapDispatchToProps = dispatch => ({
-  onSubmit: expense => dispatch(addExpense(expense))
+  addExpense: expense => dispatch(addExpense(expense))
 });
 
 export default connect(
