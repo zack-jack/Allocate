be6748ae32061710f645f4461d7c6c12dcb2fca1
zackjacksongit <z.jackson58@gmail.com>
2018-10-17 13:29:24 -0400

    Authentication reducers

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
 import { startSetExpenses } from "./actions/expenses";
+import { login, logout } from "./actions/auth";
 import AppRouter, { history } from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
 
@@ -31,6 +32,7 @@ ReactDOM.render(<p>Loading...</p>, document.getElementById("app"));
 
 firebase.auth().onAuthStateChanged(user => {
   if (user) {
+    store.dispatch(login(user.uid));
     store.dispatch(startSetExpenses()).then(() => {
       renderApp();
       if (history.location.pathname === "/") {
@@ -38,6 +40,7 @@ firebase.auth().onAuthStateChanged(user => {
       }
     });
   } else {
+    store.dispatch(logout());
     renderApp();
     history.push("/");
   }
